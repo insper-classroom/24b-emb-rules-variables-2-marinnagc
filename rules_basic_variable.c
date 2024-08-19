f66deca1d77c1fecfe709a8b4dc82714742777b9
Marinna <marinnagc@al.insper.edu.br>
2024-08-19 11:18:38 -0300

    a como ponteiro

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -5,17 +5,17 @@
 
 
 
-int foo(int) {
-    a = a + 1; // acessa variavel global
-    return a;
+void foo(int &a) {
+    *a = *a + 1; // acessa ponteiro
 }
 
 void main(void) {
-    int a = 0; 
-    int b = 0; 
+    int a = 0;
+    int b = 0;
+
     while (1) {
-        a = foo(a);
-        
+        a = foo(&a);
+
         if (a > 5) {
             b = 1;
         }
