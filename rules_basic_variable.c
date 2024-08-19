2e266b209a3b71b4a977bb7a3adb001639bf6068
Marinna <marinnagc@al.insper.edu.br>
2024-08-19 11:08:11 -0300

    arrumando

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -5,13 +5,14 @@
 int a = 0; // var global
 int b = 0; // var global
 
-void foo(void) {
+int foo(int) {
     a = a + 1; // acessa variavel global
+    return a;
 }
 
 void main(void) {
     while (1) {
-        foo();
+        a = foo(a);
         
         if (a > 5) {
             b = 1;
